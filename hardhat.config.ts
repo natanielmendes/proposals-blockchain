843f2111c87dbb42ce5f48a73719bd6634bca58a
Nataniel Mendes <natanielmendes@hotmail.com>
2025-05-26 16:11:50 -0300

    Converting CommonJS require statements to ES6 import statements

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,16 +1,19 @@
-require("@nomicfoundation/hardhat-toolbox");
-require("dotenv").config();
+import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL;
 const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
 
-/** @type import('hardhat/config').HardhatUserConfig */
-module.exports = {
-  solidity: "0.8.17",
+const config = {
+  solidity: "0.8.19",
   networks: {
     sepolia: {
       url: SEPOLIA_RPC_URL,
       accounts: [PRIVATE_KEY],
     },
   },
-}; 
+};
+
+export default config; 
